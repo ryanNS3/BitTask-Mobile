f44ccd0de965037b7853a70fac3b3d9aa43e8846
LaizaBarbosa <laiza0700@gmail.com>
2024-02-15 16:42:33 -0300

    adição do component gestureHandler

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { titulos } from './styles/tipografia';
+import VerTarefa from './screens/verTarefa';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text ></Text>
-      <Text style={titulos.h3}>Oen up App.tsx to start working on your app!</Text>
+    <GestureHandlerRootView style={{ flex:1 }}>
+      <View style={styles.container}>
+      <Text style={titulos.h3}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <VerTarefa></VerTarefa>
+      
     </View>
+    </GestureHandlerRootView>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
