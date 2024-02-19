443f9309260e07bfc778d221ba5419c900c02701
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-02-19 00:04:35 -0300

    Add files via upload

---
+++ src/screens/categorias/categorias.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../../components/header';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import TypeCategories from '../../components/typeCategories';
+
+export default function Categorias() {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* Adicionando o Header */}
+      <Header headerText={'Categorias'} />
+
+      {/* Adicionando as categorias */}
+      <View style={styles.categories}>
+        <Text style={styles.text}>Tudo</Text>
+      </View>
+
+    <TypeCategories />
+
+    </SafeAreaView>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F6F9FE',
+  },
+
+  categories: {
+    elevation: 0
+  },
+
+  text: {
+    fontFamily: 'Roboto',
+    fontSize: 20,
+    letterSpacing: 0.2,
+    lineHeight: 28,
+    fontWeight: 'bold',
+    left: '5%'
+  }
+});
