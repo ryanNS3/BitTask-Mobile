fb274c6642289af8384863975b452f7fde3c1c13
LaizaBarbosa <laiza0700@gmail.com>
2024-02-25 16:27:10 -0300

    criação dos svgs como componentes

---
+++ assets/icones/calendario.js
@@ -0,0 +1,22 @@
+import { Svg,Path, Stop, Defs,Rect } from "react-native-svg";
+
+const IconCalendario = () => {
+    return(
+        <Svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Rect width="23.4486" height="22.1696" rx="5" fill="url(#paint0_linear_1396_63)"/>
+<Path d="M14 5.66668V4.33334M14 5.66668V7.00001M14 5.66668H11M6 9.66668V15.6667C6 16.4031 6.59695 17 7.33333 17H16.6667C17.4031 17 18 16.4031 18 15.6667V9.66668H6Z" stroke="#F6F9FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M6 9.66666V6.99999C6 6.26361 6.59695 5.66666 7.33333 5.66666H8.66667" stroke="#F6F9FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M8.66699 4.33334V7.00001" stroke="#F6F9FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M17.9997 9.66666V6.99999C17.9997 6.26361 17.4027 5.66666 16.6663 5.66666H16.333" stroke="#F6F9FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<Defs>
+<linearGradient id="paint0_linear_1396_63" x1="11.7243" y1="0" x2="11.7243" y2="22.1696" gradientUnits="userSpaceOnUse">
+<Stop stop-color="#195EFC"/>
+<Stop offset="1" stop-color="#0D23ED"/>
+</linearGradient>
+</Defs>
+</Svg>
+
+ )
+}
+
+export default IconCalendario
---
+++ assets/icones/prioridade.js
@@ -0,0 +1,13 @@
+import { Svg,Path } from "react-native-svg";
+
+const IconPrioridade = () => {
+    return(
+        <Svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Path d="M9.58301 8.79166L8.33301 7.54166L7.08301 8.79166" stroke="#FFCFCF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<Path d="M5.33301 4.66666H11.333C12.4376 4.66666 13.333 5.56209 13.333 6.66666V9.66666C13.333 10.7712 12.4376 11.6667 11.333 11.6667H5.33301C4.22844 11.6667 3.33301 10.7712 3.33301 9.66666V6.66666C3.33301 5.56209 4.22844 4.66666 5.33301 4.66666Z" stroke="#FFCFCF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</Svg>
+
+    )
+}
+
+export default IconPrioridade
